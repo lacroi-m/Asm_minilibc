19a35884b6d5dee882f0c4e6c27df5db437527b5
Maxime Lacroix <maxime.lacroix@epitech.eu>
2017-03-22 19:56:14 +0100

    ok, redid it cuz that was crap + rename to strcasecmp and not my_strcasecmp

--- sources/strcasecmp.asm
+++ sources/strcasecmp.asm
@@ -1,12 +1,10 @@
 BITS	64
-section .text
-	global	my_strcasecmp:function ;int strcasecmp(const char *s1, const char *s2);
-my_strcasecmp:			       ;		  s1->rdi	s2->rsi
+section .text	
+	global	strcasecmp:function ;int strcasecmp(const char *s1, const char *s2);
+strcasecmp:			       ;		  s1->rdi	s2->rsi
 	PUSH	rbp
 	MOV	rbp, rsp	    ;prologue
 
-	XOR	r9, r9		;for fucking compare
-	XOR	rcx, rcx
 	XOR	r8, r8		    ;(registre general)for stocking caracters of s1
 	XOR	rbx, rbx	    ;(registre general)for stocking caracters of s2
 loop:
@@ -14,16 +12,19 @@ loop:
 	MOV	bl, [rsi]
 
 	CMP	r8b, 0
-	JE	diff
+	JE	is_eq
 	
 	CMP	bl, r8b
 	JNE	maj_upp
 	JE	_inc
-	
+is_eq:
+	CMP	bl, 0
+	JE	_eq
+	JNE	diff
 maj_upp:
 	CMP	r8b, "A"
 	JGE	maj_low
-	JMP	diff
+	JL	diff
 maj_low:
 	CMP	r8b, "Z"
 	JLE	mini
@@ -31,36 +32,37 @@ maj_low:
 min_upp:
 	CMP	r8b, "a"
 	JGE	min_low
-	JMP	diff
+	JL	diff
 min_low:
 	CMP	r8b, "z"
 	JLE	maji
-	Jmp	diff
+	JG	diff
 
 mini:
-	CMP	r9b, -32
+	ADD	r8b, 32
+	CMP	r8b, bl
 	JE	_inc
-	JNE	diff
+	JNE	min
+min:
+	SUB	r8b, 32
+	JMP	diff
+
 maji:
-	CMP	r9b, 32
+	ADD	bl, 32
+	CMP	bl, r8b
 	JE	_inc
-	JNE	diff	;everything is going here
+	JNE	mam	;everything is going here
+mam:
+	SUB	bl, 32
+	JMP	diff
 _inc:
-	INC	rcx
 	INC	rdi
 	INC	rsi
 	JMP	loop
 
-hacks:
-	MOV	rax, 0
-	JMP	prologue
 diff:
 	SUB	r8, rbx
 	MOV	rax, r8
-	CMP	rax, 32
-	JE	hacks
-	CMP	rax, -32
-	JE	hacks
 	JMP	prologue
 diff_t:
 	MOV	rax, 2222
