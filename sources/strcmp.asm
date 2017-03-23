7e686d717f5b68d094d7e33355b7d6a196be4a68
Maxime Lacroix <maxime.lacroix@epitech.eu>
2017-03-23 18:53:46 +0100

    strcmp redone...

--- sources/strcmp.asm
+++ sources/strcmp.asm
@@ -1,35 +1,37 @@
 BITS	64
-section .text
-	global	strcmp:function ;strcmp(const char *s1, const char *s2)
-
-strcmp:			    ;		s1->rdi		s2->rsi
+section	.text
+	global	strcmp:function
+strcmp:
 	PUSH	rbp
-	MOV	rbp, rsp	    ;prologue
+	MOV	rbp, rsp
 
-	XOR	rbx, rbx	    ;(registre general)for stocking caracters of s2
-	XOR	r8, r8		    ;(registre general)for stocking caracters of s1
+	XOR	rcx, rcx	;compteur int
+	XOR	bl, bl
 loop:
-	MOV	bl, [rsi]	    ;stokage du char dans partie basse du registre(8BITS) de rbx
-	MOV	r8b, [rdi]	    ;stokage du char dans partie basse du registre(8BITS) de r8
-	
-	CMP	r8b, bl 	    ;comapare s1[x] and s2[x]
-	JNE	diff		    ;if != goto diff
-	CMP	r8b, 0		    ;verification que s1 != NULL
-	JE	diff
-	CMP	bl, 0		    ;verification que s2 != NULL
-	JE	diff
-	INC	rdi		    ;incrementer adresse de bloque du s1
-	INC	rsi		    ;incrementer adresse de bloque du s2
+	CMP	[rdi + rcx], BYTE 0
+	JE	_eq
+	MOV	bl, [rsi]
+	CMP	[rdi + rcx], bl
+	JNE	diff
+	INC	rcx
+	INC	rsi
 	JMP	loop
 
 diff:
-	SUB	r8, rbx		;faire la difference entre
-	mov	rax, r8		; déplacement du résultat dans rax
+	CMP	[rdi + rcx], bl
+	JL	less
+	CMP	[rdi + rcx], bl
+	JG	greater
+less:
+	MOV	rax, -1
+	JMP	prologue
+greater:
+	MOV	rax, 1
 	JMP	prologue
 _eq:
-	MOV	rax, 0		;pour etre sur de retourner 0
+	MOV	rax, 0
 	JMP	prologue
 prologue:
-	MOV     rsp, rbp
-	POP     rbp
-	RET			;return
+	MOV	rsp, rbp
+	POP	rbp
+	RET
